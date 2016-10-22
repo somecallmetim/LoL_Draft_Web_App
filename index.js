3041a0f510f2ce5de0ec1c55c365cc474968e5e7
Tim Bauer <timbauer@ymail.com>
2016-10-21 18:34:20 -0700

    first iteration of layout

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ $(document).ready(function() {
 
     var ddragonVersion;
     var champImgUrl;
+    var gridColumnArray = ["#imageList1", "#imageList2", "#imageList3", "#imageList4", "#imageList5", "#imageList6"];
     $.ajax({
         url:"https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key=8de9b045-bbd0-4c39-9a8f-c4dfd32e157b",
         datatype: "json",
         success: function(response){
             ddragonVersion = response[0];
-            //console.log(ddragonVersion);
+
+            champImgUrl = "http://ddragon.leagueoflegends.com/cdn/" + ddragonVersion + "/img/champion/Jinx.png";
+            $(".blue-side").append('<img class="icon" src="' + champImgUrl + '" />');
+            champImgUrl = "http://ddragon.leagueoflegends.com/cdn/" + ddragonVersion + "/img/champion/Caitlyn.png";
+            $(".blue-side").append('<img class="icon" src="' + champImgUrl + '" />');
+
+            champImgUrl = "http://ddragon.leagueoflegends.com/cdn/" + ddragonVersion + "/img/champion/Caitlyn.png";
+            $(".red-side").append('<img class="icon" src="' + champImgUrl + '" />'); 
+            champImgUrl = "http://ddragon.leagueoflegends.com/cdn/" + ddragonVersion + "/img/champion/Jinx.png";  
+            $(".red-side").append('<img class="icon" src="' + champImgUrl + '" />');        
         },
         error: function(error){
             console.log(error)
@@ -18,14 +28,18 @@ $(document).ready(function() {
         url: "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key=8de9b045-bbd0-4c39-9a8f-c4dfd32e157b",
         datatype: "json",
         success: function(response){
+            var iterator = 0;
             $.each(response.data, function(index, champion){
-                //debugger;
+                iterator++;
+                if (iterator > 5){iterator = 0};
+
                 champImgUrl = "http://ddragon.leagueoflegends.com/cdn/" + ddragonVersion + "/img/champion/" + champion.image.full;
-                //console.log(champImgUrl);
-                $("#imageList").append('<img id="' + champion + '" src="' + champImgUrl + '" />');
+                $(gridColumnArray[iterator]).append('<img id="' + champion.name + '"class=\'icon\' src="' + champImgUrl + '" />');
             })
         }
     })
-});
 
+});
+//debugger;
+//console.log(champImgUrl);
 //http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/
