14b68a09f666d856989cb5f07cd212ab4c873bdc
Tim Bauer <timbauer@ymail.com>
2016-10-22 22:38:53 -0700

    escape key now resets champ-list, you can now only select one champ at a time, other small code cleanup

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     var bluePicks = 0;
     var redBans = 0;
     var blueBans = 0;
+    var isAnIconActive = false;
     $.ajax({
         url:"https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?api_key=8de9b045-bbd0-4c39-9a8f-c4dfd32e157b",
         datatype: "json",
@@ -45,10 +46,7 @@ $(document).ready(function() {
                 var imageFileName = champList[champName].image.full;
                 var champIcon;
 
-                champImgUrl = "http://ddragon.leagueoflegends.com/cdn/" + ddragonVersion + "/img/champion/" + imageFileName;
-
-                champIcon = '<img id="' + champName + '"class=\'icon\' src="' + champImgUrl + '" />'
-                $(gridColumnArray[iterator]).append(champIcon);
+                getIconImage(champName, gridColumnArray[iterator], false);
 
                 setIconAsClickable(champName);
 
@@ -66,77 +64,103 @@ $(document).ready(function() {
             //console.log(event.relatedTarget.id);
 
             var champId = event.relatedTarget.id;
-            champId = champId.replace("_New", "");
-
-            $(event.relatedTarget).remove();
+            removeIcon(event.relatedTarget.id);
+            var champId = champId.replace("_Temp", "");
 
             switch (event.target.id) {
                 case "blue-side-container":
                     if(bluePicks < 5){
-                        getIconImage(champId, "#blue-side-picks");
+                        getIconImage(champId, "#blue-side-picks", true);
                         bluePicks++;
+                        $("#" + champId).css("opacity", 0.5);
+                        $("#" + champId).off();
                     }else {
                         $("#" + champId).css("opacity", 1);
-                        setIconAsClickable(champId);
                     }
                 break;
                 case "red-side-container":
                     if(redPicks < 5) {
-                        getIconImage(champId, "#red-side-picks");
+                        getIconImage(champId, "#red-side-picks", true);
                         redPicks++;
+                        $("#" + champId).css("opacity", 0.5);
+                        $("#" + champId).off();
                     }else {
                         $("#" + champId).css("opacity", 1);
-                        setIconAsClickable(champId);
                     }
                 break;
                 case "blue-side-bans-container":
                     if(blueBans < 3){
-                        getIconImage(champId, "#blue-side-bans");
+                        getIconImage(champId, "#blue-side-bans", true);
                         blueBans++;
+                        $("#" + champId).css("opacity", 0.5);
+                        $("#" + champId).off();
                     }else {
                         $("#" + champId).css("opacity", 1);
-                        setIconAsClickable(champId);
                     }
                 break;
                 case "red-side-bans-container":
                     if(redBans < 3){
-                        getIconImage(champId, "#red-side-bans");                        
+                        getIconImage(champId, "#red-side-bans", true);                        
                         redBans++;
+                        $("#" + champId).css("opacity", 0.5);
+                        $("#" + champId).off();
                     }else {
                         $("#" + champId).css("opacity", 1);
-                        setIconAsClickable(champId);
                     }
 
                 break;
                 default:
                 break;
             }
-    }
+            
+        }
     });
 
-    function getIconImage (champId, listToAddId){
+    function getIconImage (champId, listToAddId, isPickOrBan){
 
         var imageFileName = champList[champId].image.full;
+        var champIcon;
         champImgUrl = "http://ddragon.leagueoflegends.com/cdn/" + ddragonVersion + "/img/champion/" + imageFileName;
 
-        champIcon = '<img id="' + champId + '"class=\'icon\' src="' + champImgUrl + '" />'
+        if(isPickOrBan){
+            champIcon = '<img id="' + champId + '_New" class="icon" src="' + champImgUrl + '" />'
+        }else {
+            champIcon = '<img id="' + champId + '"class="icon" src="' + champImgUrl + '" />'
+        }
+
         $(listToAddId).append(champIcon);
         
+    }
+
+    function removeIcon(champId) {
+        isAnIconActive = false;
+        var champId = "#" + champId;
+        $(champId).remove();
+        $(document).off();
     }
 
     function setIconAsClickable(champId){
         champId = "#" + champId;
         $(champId).click(function(e){
-            var newId = this.id + "_New"
-            $(champId).clone().prop('id', newId).appendTo("#champ-select").offset({left:e.pageX,top:e.pageY});
+            if(!isAnIconActive){
+                isAnIconActive = true;
+                var newId = this.id + "_Temp"
+                $(champId).clone().prop('id', newId).appendTo("#champ-select").offset({left:e.pageX,top:e.pageY});
 
 
-            interact("#" + newId).draggable({
-                onmove: dragMoveListener,
-            });
+                interact("#" + newId).draggable({
+                    onmove: dragMoveListener,
+                    onend: function(event){
+                        removeIcon(event.target.id);
+                    }
+                });
 
-            $(champId).css("opacity", 0.5);
-            $(champId).off();
+                $(document).keydown(function(e){
+                    if(e.keyCode == 27){ // escape key maps to keycode `27`
+                        removeIcon(newId);
+                    }
+                })
+            }
         });
     }
     
